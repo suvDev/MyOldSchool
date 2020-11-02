b3b659c6f577931acea4bab8ae5ff45da64ed774
suvdev.work <suvdev.work@gmail.com>
2020-11-02 13:39:41 +0530

    added eureka server dependency

--- src/main/java/com/myoldschool/manager/ManagerApplication.java
+++ src/main/java/com/myoldschool/manager/ManagerApplication.java
@@ -3,11 +3,14 @@ package com.myoldschool.manager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@EnableEurekaServer
 public class ManagerApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("spring.config.name", "registration-server");
 		SpringApplication.run(ManagerApplication.class, args);
 	}
 

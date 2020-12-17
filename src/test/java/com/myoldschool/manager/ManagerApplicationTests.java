b455b002c69b1ef86908fbe5da25015d9222dbec
suvdev.work <suvdev.work@gmail.com>
2020-12-17 19:49:17 +0530

    added test case for post request of adding users

--- src/test/java/com/myoldschool/manager/ManagerApplicationTests.java
+++ src/test/java/com/myoldschool/manager/ManagerApplicationTests.java
@@ -4,21 +4,25 @@ import com.myoldschool.manager.api.ApiController;
 import com.myoldschool.manager.api.Student;
 import com.netflix.ribbon.proxy.annotation.Http;
 import org.checkerframework.checker.units.qual.A;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.BDDMockito.given;
@@ -26,6 +30,7 @@ import static org.mockito.BDDMockito.given;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ManagerApplicationTests {
 
     @Autowired
@@ -34,14 +39,17 @@ class ManagerApplicationTests {
     @MockBean
     private ApiController controller;
 
-    @Test
-    void testGetAllRecords() {
+    @BeforeAll
+    public void mockResponseForApis(){
         Student student = new Student(
                 MockConstants.id, MockConstants.name, MockConstants.rollNo, MockConstants.marks);
         ArrayList<Student> mockList = new ArrayList<>();
-		mockList.add(student);
+        mockList.add(student);
         given(controller.getAllRecords()).willReturn(mockList);
+    }
 
+    @Test
+    void testGetAllRecords() {
         ResponseEntity<ArrayList<Student>> responseEntity = testRestTemplate.exchange("/getRecords",
                 HttpMethod.GET, null, new ParameterizedTypeReference<ArrayList<Student>>() {
                 });
@@ -52,6 +60,21 @@ class ManagerApplicationTests {
         assertThat(list.get(0).getName()).isEqualTo(MockConstants.name);
         assertThat(list.get(0).getRollno()).isEqualTo(MockConstants.rollNo);
         assertThat(list.get(0).getMarks()).isEqualTo(MockConstants.marks);
+    }
+
+    @Test
+    void testPostUser(){
+
+        Student student = new Student(2, "Adam", 102, "100");
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<Student> request = new HttpEntity<>(student, headers);
+
+        ResponseEntity<Map> result = testRestTemplate.postForEntity("/postUser", request, Map.class);
+        assertThat(result.getStatusCode()).isEqualTo(HttpStatus.OK);
+
     }
 
 }
